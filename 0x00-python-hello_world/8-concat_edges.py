ec1537db9d2370c0f08d8bcdbb00aa7691ca4b06
javalytic <ohluwacheunjava@gmail.com>
2022-08-22 23:44:47 -0700

    fix

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
         str3 = str[39:67] + str[107:112] + str[:6]
-        print(str3)
+print(str3)
