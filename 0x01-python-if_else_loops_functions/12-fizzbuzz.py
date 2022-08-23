7c2664b00d773c40fdc22f881c6b2822ca2c6461
javalytic <ohluwacheunjava@gmail.com>
2022-08-23 08:06:48 -0700

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,5 +8,5 @@ def fizzbuzz():
         if i % 5 == 0:
             print("Buzz", end="")
         if i % 3 != 0 and i % 5 != 0:
-            print({:d}".format(i), end="")
+            print("{:d}".format(i), end="")
         print(" ", end="")
